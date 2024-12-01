30acc2d648ba4f6ed219ae7b8a3113be4c876b99
atom-wintermute <maxim.nikulin@bolt.eu>
2024-12-01 11:09:22 +0000

    Day 1, part 1

--- Sources/Day00.swift
+++ Sources/Day00.swift
@@ -1,25 +1,41 @@
 import Algorithms
 
 struct Day00: AdventDay {
-  // Save your data in a corresponding text file in the `Data` directory.
-  var data: String
+    // Save your data in a corresponding text file in the `Data` directory.
+    var data: String
 
-  // Splits input data into its component parts and convert from string.
-  var entities: [[Int]] {
-    data.split(separator: "\n\n").map {
-      $0.split(separator: "\n").compactMap { Int($0) }
+    // Splits input data into its component parts and convert from string.
+    var entities: [[Int]] {
+        data.split(separator: "\n").map {
+            return $0.split(separator: "   ").compactMap {
+                return Int($0)
+            }
+        }
     }
-  }
 
-  // Replace this with your solution for the first part of the day's challenge.
-  func part1() -> Any {
-    // Calculate the sum of the first set of input data
-    entities.first?.reduce(0, +) ?? 0
-  }
+    // Replace this with your solution for the first part of the day's challenge.
+    func part1() -> Any {
+        var firstList = [Int]()
+        var secondList = [Int]()
 
-  // Replace this with your solution for the second part of the day's challenge.
-  func part2() -> Any {
-    // Sum the maximum entries in each set of data
-    entities.map { $0.max() ?? 0 }.reduce(0, +)
-  }
+        for entity in entities {
+            firstList.append(entity[0])
+            secondList.append(entity[1])
+        }
+
+        firstList.sort()
+        secondList.sort()
+
+        var sum = 0
+        for (index, dist) in firstList.enumerated() {
+            sum += abs(dist - secondList[index])
+        }
+
+        return sum
+    }
+
+    // Replace this with your solution for the second part of the day's challenge.
+    func part2() -> Any {
+        return false
+    }
 }
