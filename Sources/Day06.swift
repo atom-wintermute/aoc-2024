c51ec1ceb56a627fcaa67b101a9ffe268d8d5418
atom-wintermute <maxim.nikulin@bolt.eu>
2024-12-15 23:46:28 +0400

    Day 6, part 2

--- Sources/Day06.swift
+++ Sources/Day06.swift
@@ -107,8 +107,180 @@ struct Day06: AdventDay {
 		return path.count
 	}
 
+	private struct PositionDirection: Hashable {
+		let x: Int
+		let y: Int
+		let direction: Direction
+	}
+
 	// Replace this with your solution for the second part of the day's challenge.
 	func part2() -> Any {
-		return false
+		var initialPosition = PositionDirection(x: 0, y: 0, direction: .right)
+
+		for (i, entity) in entities.enumerated() {
+			for (j, char) in entity.enumerated() {
+				if char == "<" {
+					initialPosition = PositionDirection(x: j, y: i, direction: .left)
+					break
+				} else if char == ">" {
+					initialPosition = PositionDirection(x: j, y: i, direction: .right)
+					break
+				} else if char == "^" {
+					initialPosition = PositionDirection(x: j, y: i, direction: .up)
+					break
+				} else if char == "v" {
+					initialPosition = PositionDirection(x: j, y: i, direction: .down)
+					break
+				}
+			}
+		}
+
+		var position = Position(x: initialPosition.x, y: initialPosition.y)
+		var direction = initialPosition.direction
+		var path = Set<Position>()
+		path.insert(position)
+		var outOfBounds = false
+		while(!outOfBounds) {
+
+			switch direction {
+			case .up:
+				if position.y == 0 {
+					outOfBounds = true
+					continue
+				}
+				if entities[position.y - 1][position.x] == "#" {
+					direction = .right
+					continue
+				}
+				position = Position(x: position.x, y: position.y - 1)
+				path.insert(position)
+			case .down:
+				if position.y == entities.count - 1 {
+					outOfBounds = true
+					continue
+				}
+				if entities[position.y + 1][position.x] == "#" {
+					direction = .left
+					continue
+				}
+				position = Position(x: position.x, y: position.y + 1)
+				path.insert(position)
+			case .left:
+				if position.x == 0 {
+					outOfBounds = true
+					continue
+				}
+				if entities[position.y][position.x - 1] == "#" {
+					direction = .up
+					continue
+				}
+				position = Position(x: position.x - 1, y: position.y)
+				path.insert(position)
+			case .right:
+				if position.x == entities[0].count - 1 {
+					outOfBounds = true
+					continue
+				}
+				if entities[position.y][position.x + 1] == "#" {
+					direction = .down
+					continue
+				}
+				position = Position(x: position.x + 1, y: position.y)
+				path.insert(position)
+			}
+		}
+
+		var sum = 0
+		for location in path {
+
+			var newObstacle = Position(x: 0, y: 0)
+			if entities[location.y][location.x] == "." {
+				newObstacle = Position(x: location.x, y: location.y)
+			} else {
+				continue
+			}
+
+			var position = initialPosition
+			var path = Set<PositionDirection>()
+			path.insert(position)
+
+			var samePath = false
+			var outOfBounds = false
+
+			while(!samePath && !outOfBounds) {
+				switch position.direction {
+				case .up:
+					if position.y == 0 {
+						outOfBounds = true
+						continue
+					}
+					if entities[position.y - 1][position.x] == "#" ||
+						(position.y - 1 == newObstacle.y && position.x == newObstacle.x) {
+						position = PositionDirection(x: position.x, y: position.y, direction: .right)
+						continue
+					}
+					position = PositionDirection(x: position.x, y: position.y - 1, direction: position.direction)
+					if path.contains(position) {
+						samePath = true
+						sum += 1
+						continue
+					}
+					path.insert(position)
+				case .down:
+					if position.y == entities.count - 1 {
+						outOfBounds = true
+						continue
+					}
+					if entities[position.y + 1][position.x] == "#" ||
+						(position.y + 1 == newObstacle.y && position.x == newObstacle.x) {
+						position = PositionDirection(x: position.x, y: position.y, direction: .left)
+						continue
+					}
+					position = PositionDirection(x: position.x, y: position.y + 1, direction: position.direction)
+					if path.contains(position) {
+						samePath = true
+						sum += 1
+						continue
+					}
+					path.insert(position)
+				case .left:
+					if position.x == 0 {
+						outOfBounds = true
+						continue
+					}
+					if entities[position.y][position.x - 1] == "#" ||
+						(position.y == newObstacle.y && position.x - 1 == newObstacle.x) {
+						position = PositionDirection(x: position.x, y: position.y, direction: .up)
+						continue
+					}
+					position = PositionDirection(x: position.x - 1, y: position.y, direction: position.direction)
+					if path.contains(position) {
+						samePath = true
+						sum += 1
+						continue
+					}
+					path.insert(position)
+				case .right:
+					if position.x == entities[0].count - 1 {
+						outOfBounds = true
+						continue
+					}
+					if entities[position.y][position.x + 1] == "#" ||
+						(position.y == newObstacle.y && position.x + 1 == newObstacle.x) {
+						position = PositionDirection(x: position.x, y: position.y, direction: .down)
+						continue
+					}
+					position = PositionDirection(x: position.x + 1, y: position.y, direction: position.direction)
+					if path.contains(position) {
+						samePath = true
+						sum += 1
+						continue
+					}
+					path.insert(position)
+				}
+			}
+		}
+
+		return sum
 	}
 }
