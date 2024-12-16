b32ee832821594f2cb3d74d347a487527d6dc26b
atom-wintermute <maxim.nikulin@bolt.eu>
2024-12-16 08:19:13 +0400

    Day 7, part 2

--- Sources/Day07.swift
+++ Sources/Day07.swift
@@ -17,6 +17,12 @@ struct Day07: AdventDay {
 		case add
 	}
 
+	enum Operation2 {
+		case multiply
+		case add
+		case concatenation
+	}
+
 	// Replace this with your solution for the first part of the day's challenge.
 	func part1() -> Any {
 		var sum = 0
@@ -75,6 +81,75 @@ struct Day07: AdventDay {
 
 	// Replace this with your solution for the second part of the day's challenge.
 	func part2() -> Any {
-		return false
+		var sum = 0
+
+		for entity in entities {
+			let splitted = entity.split(separator: ":")
+			let result = Int(splitted[0])!
+
+			let numbers = splitted[1].split(separator: " ").map { Int($0)! }
+
+			let results = getPossibleResults2(for: numbers, operation: .add)
+			let results2 = getPossibleResults2(for: numbers, operation: .multiply)
+			let results3 = getPossibleResults2(for: numbers, operation: .concatenation)
+
+			if results.contains(result) || results2.contains(result) || results3.contains(result) {
+				sum += result
+			}
+		}
+
+		return sum
+	}
+
+	private func getPossibleResults2(for numbers: [Int], operation: Operation2) -> [Int] {
+		if numbers.count == 2 {
+			switch operation {
+			case .multiply:
+				return [numbers[0] * numbers[1]]
+			case .add:
+				return [numbers[0] + numbers[1]]
+			case .concatenation:
+				let left = String(numbers[0])
+				let right = String(numbers[1])
+				return [Int(left + right)!]
+			}
+		} else {
+			let last = numbers.last!
+			var results = [Int]()
+			switch operation {
+			case .multiply:
+				let remainNumbers = Array(numbers.dropLast())
+				var others = [Int]()
+				others += getPossibleResults2(for: remainNumbers, operation: .multiply)
+				others += getPossibleResults2(for: remainNumbers, operation: .add)
+				others += getPossibleResults2(for: remainNumbers, operation: .concatenation)
+				for other in others {
+					results.append(other * last)
+				}
+				return results
+			case .add:
+				let remainNumbers = Array(numbers.dropLast())
+				var others = [Int]()
+				others += getPossibleResults2(for: remainNumbers, operation: .multiply)
+				others += getPossibleResults2(for: remainNumbers, operation: .add)
+				others += getPossibleResults2(for: remainNumbers, operation: .concatenation)
+				for other in others {
+					results.append(other + last)
+				}
+				return results
+			case .concatenation:
+				let remainNumbers = Array(numbers.dropLast())
+				var others = [Int]()
+				others += getPossibleResults2(for: remainNumbers, operation: .multiply)
+				others += getPossibleResults2(for: remainNumbers, operation: .add)
+				others += getPossibleResults2(for: remainNumbers, operation: .concatenation)
+				for other in others {
+					let left = String(other)
+					let right = String(last)
+					results.append(Int(left + right)!)
+				}
+				return results
+			}
+		}
 	}
 }
