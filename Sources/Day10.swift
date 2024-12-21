e3366a2b52447cead7e5109a0421a4e8f78f5c16
atom-wintermute <maxim.nikulin@bolt.eu>
2024-12-21 13:04:32 +0400

    Day 10, part 2

--- Sources/Day10.swift
+++ Sources/Day10.swift
@@ -92,6 +92,67 @@ struct Day10: AdventDay {
 
     // Replace this with your solution for the second part of the day's challenge.
     func part2() -> Any {
-        return false
+        var trail = [[Int]]()
+        for row in rows {
+            var trailRow = [Int]()
+            for symbol in row {
+                if symbol == "." {
+                    trailRow.append(-1)
+                } else {
+                    trailRow.append(Int(String(symbol))!)
+                }
+            }
+            trail.append(trailRow)
+        }
+
+        var scores = 0
+        for (y, row) in trail.enumerated() {
+            for (x, height) in row.enumerated() {
+                if height == 0 {
+                    scores += findTrailsCountFrom(trail: trail, height: 0, x: x, y: y)
+                }
+            }
+        }
+
+        return scores
+    }
+
+    private func findTrailsCountFrom(trail: [[Int]], height: Int, x: Int, y: Int) -> Int {
+
+        if height == 9 {
+            return 1
+        }
+
+        var trailsCount = 0
+
+        // up
+        if y > 0 {
+            if trail[y - 1][x] == height + 1 {
+                trailsCount += findTrailsCountFrom(trail: trail, height: height + 1, x: x, y: y - 1)
+            }
+        }
+
+        // down
+        if y < trail.count - 1 {
+            if trail[y + 1][x] == height + 1 {
+                trailsCount += findTrailsCountFrom(trail: trail, height: height + 1, x: x, y: y + 1)
+            }
+        }
+
+        // left
+        if x > 0 {
+            if trail[y][x - 1] == height + 1 {
+                trailsCount += findTrailsCountFrom(trail: trail, height: height + 1, x: x - 1, y: y)
+            }
+        }
+
+        // right
+        if x < trail[y].count - 1 {
+            if trail[y][x + 1] == height + 1 {
+                trailsCount += findTrailsCountFrom(trail: trail, height: height + 1, x: x + 1, y: y)
+            }
+        }
+
+        return trailsCount
     }
 }
