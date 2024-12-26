191f63f2c264f64d9805eebc2f256fc9ab22e41e
atom-wintermute <maxim.nikulin@bolt.eu>
2024-12-26 12:35:29 +0200

    Day 22, part 2

--- Sources/Day22.swift
+++ Sources/Day22.swift
@@ -56,8 +56,92 @@ struct Day22: AdventDay {
         return secret % 16777216
     }
 
+    private struct Changes: Hashable {
+        let first: Int
+        let second: Int
+        let third: Int
+        let fourth: Int
+    }
+
     // Replace this with your solution for the second part of the day's challenge.
     func part2() -> Any {
-        return false
+        var buyers = [Int]()
+
+        for entity in entities {
+            buyers.append(Int(entity)!)
+        }
+
+        var globalDict = [Changes: Int]()
+        for buyer in buyers {
+            var currentSecret = buyer
+
+            var dict = [Changes: Int]()
+            var prices = [Int]()
+            prices.append(currentSecret)
+            for _ in 1...2000 {
+
+                // 1
+                var value = currentSecret * 64
+                var calculatation = 0
+                calculatation = mix(value: value, secret: currentSecret)
+                currentSecret = prune(secret: calculatation)
+
+                // 2
+                value = currentSecret / 32
+                calculatation = mix(value: value, secret: currentSecret)
+                currentSecret = prune(secret: calculatation)
+
+                // 3
+                value = currentSecret * 2048
+                calculatation = mix(value: value, secret: currentSecret)
+                currentSecret = prune(secret: calculatation)
+
+                prices.append(currentSecret)
+            }
+
+            var changes = [Int]()
+            var previousPrice = prices.first! % 10
+            for (i, price) in prices.enumerated() {
+                let priceEnd = price % 10
+                if i == 0 {
+                    continue
+                }
+                changes.append(priceEnd - previousPrice)
+
+                previousPrice = priceEnd
+            }
+
+            for (i, change) in changes.enumerated() {
+                if i > 3 {
+                    let changes = Changes(
+                        first: changes[i - 3],
+                        second: changes[i - 2],
+                        third: changes[i - 1],
+                        fourth: change
+                    )
+                    let price = prices[i+1] % 10
+                    if dict[changes] == nil {
+                        dict[changes] = price
+                    }
+                }
+            }
+
+            for (changes, price) in dict {
+                if let savedPrice = globalDict[changes] {
+                    globalDict[changes] = savedPrice + price
+                } else {
+                    globalDict[changes] = price
+                }
+            }
+        }
+
+        var max = 0
+        for (_, price) in globalDict {
+            if price > max {
+                max = price
+            }
+        }
+
+        return max
     }
 }
--- Tests/Day22.swift
+++ Tests/Day22.swift
@@ -13,13 +13,20 @@ struct Day22Tests {
 2024
 """
 
+    let testData2 = """
+1
+2
+3
+2024
+"""
+
     @Test func testPart1() async throws {
         let challenge = Day22(data: testData)
         #expect(String(describing: challenge.part1()) == "37327623")
     }
 
     @Test func testPart2() async throws {
-        let challenge = Day22(data: testData)
-        #expect(String(describing: challenge.part2()) == "55312")
+        let challenge = Day22(data: testData2)
+        #expect(String(describing: challenge.part2()) == "23")
     }
 }
